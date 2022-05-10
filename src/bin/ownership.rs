cc912f28ab639c663c7af4cce6a83b4721786833
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-11 08:40:53 +0900

    Fix

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -4,3 +4,29 @@ fn main() {
     *bx = 200;
     println!("{}", x);
 }
+
+fn i32_case() {
+    let x = 100;
+    i_taker(x);
+    println!("{}", x);
+}
+
+fn string_case() {
+    let s = String::from("hello");
+    // s_taker(s);
+    // println!("{}", s); // couldn't
+    s_borrower(&s);
+    println!("{}", s);
+}
+
+fn i_taker(x: i32){
+    println!("{}", x);
+}
+
+fn s_taker(s: String) {
+    println!("{}", s)
+}
+
+fn s_borrower(s: &String) {
+    println!("{}", s)
+}
