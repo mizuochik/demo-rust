15c1e0ab3aba86334bfed0262d2933b7aed97fdf
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-10 12:50:38 +0900

    Fix

--- src/bin/control_flow.rs
+++ src/bin/control_flow.rs
@@ -1,8 +1,12 @@
 fn main() {
-    let n = 3;
+    let n= 3;
     if n > 0 {
         println!("> 0");
     } else {
         println!("<= 0");
     }
+
+    let m = 3;
+    let n = if m > 0 { 3 } else { 0 };
+    println!("{}", n);
 }
