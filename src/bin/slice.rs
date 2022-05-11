aa6eec7033aaf187f250b47ce9f98be6db6b5b8a
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-05-11 09:16:40 +0900

    Fix

--- src/bin/slice.rs
+++ src/bin/slice.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let bs = "hogehoge".as_bytes();
-    let b = bs[0];
-    println!("{}", b);
+    let b = "hogehoge".as_bytes();
+    let bv = b[0];
+    let bs = &b[0..2];
+    println!("{}", bv);
+    println!("{}", bs[0]);
+    iter(b);
 }
 
+fn iter(b: &[u8]) {
+    for v in b.iter() {
+        println!("{}", v);
+    }
+}
