0898dfff7d8282227a47ef12b9e8dbe20b79c473
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-03 18:41:25 +0900

    Fix

--- demo-tokio/src/bin/spawning.rs
+++ demo-tokio/src/bin/spawning.rs
@@ -1,4 +1,6 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{Connection, Frame, Command::{self, Set, Get}};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -14,9 +16,22 @@ async fn main() {
 
 async fn process(socket: TcpStream) {
     let mut connection = Connection::new(socket);
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("Got: {:?}", frame);
-        let response = Frame::Error(String::from("unimplemented"));
+    let mut db = HashMap::new();
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple(String::from("OK"))
+            },
+            Get(cmd) => {
+                    if let Some(value) = db.get(cmd.key()) {
+                        Frame::Bulk(value.clone().into())
+                    } else {
+                        Frame::Null
+                    }
+            },
+            cmd => panic!("unimplemented {:?}", cmd)
+        };
         connection.write_frame(&response).await.unwrap();
     }
 }
