958742f422e74a5d81a88b6e8d6428545297084a
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-04 00:16:48 +0900

    Add dyanmic_di

---
+++ demo-di/src/bin/dynamic_di.rs
@@ -0,0 +1,67 @@
+use std::sync::Arc;
+
+pub trait Database {
+    fn select(&self) -> String;
+}
+
+pub struct DatabaseImpl {}
+
+impl Database for DatabaseImpl {
+    fn select(&self) -> String {
+        String::from("<selected>")
+    }
+}
+
+pub trait UseCase {
+    fn run(&self);
+}
+
+pub struct UseCaseImpl {
+    database: Arc<dyn Database>,
+}
+
+impl UseCase for UseCaseImpl {
+    fn run(&self) {
+        let msg = self.database.select();
+        println!("selected {}", msg);
+    }
+}
+
+pub trait Handler {
+    fn handle(&self);
+}
+
+pub struct HandlerImpl {
+    use_case: Arc<dyn UseCase>,
+}
+
+impl Handler for HandlerImpl {
+    fn handle(&self) {
+        self.use_case.run();
+    }
+}
+
+pub struct Di {
+    pub database: Arc<dyn Database>,
+    pub use_case: Arc<dyn UseCase>,
+    pub handler: Arc<dyn Handler>,
+}
+
+pub fn new_di() -> Di {
+    let db = Arc::new(DatabaseImpl {});
+    let uc = Arc::new(UseCaseImpl {
+        database: db.clone(),
+    });
+    let h = Arc::new(HandlerImpl {
+        use_case: uc.clone(),
+    });
+    Di {
+        database: db.clone(),
+        use_case: uc.clone(),
+        handler: h.clone(),
+    }
+}
+
+fn main() {
+    new_di().handler.handle();
+}
