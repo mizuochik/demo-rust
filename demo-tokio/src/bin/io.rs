8c39c7a10beff5e371e1b9d20dfd171a41078274
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-04 22:27:23 +0900

    Add

--- demo-tokio/src/bin/io.rs
+++ demo-tokio/src/bin/io.rs
@@ -1,12 +1,14 @@
 use tokio::{
     fs::File,
-    io::{self, AsyncReadExt},
+    io::{self, AsyncReadExt, AsyncWriteExt},
 };
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    read().await.unwrap();
-    read_to_end().await
+    read().await?;
+    read_to_end().await?;
+    write_all().await?;
+    Ok(())
 }
 
 async fn read() -> io::Result<()> {
@@ -14,6 +16,7 @@ async fn read() -> io::Result<()> {
     let mut buf = [0; 10];
     let n = f.read(&mut buf).await?;
     println!("0..{} {:?}", n, &buf[..n]);
+    read_to_end().await?;
     Ok(())
 }
 
@@ -22,5 +25,11 @@ async fn read_to_end() -> io::Result<()> {
     let mut buf = Vec::new();
     let n = f.read_to_end(&mut buf).await?;
     println!("0..{} {:?}", n, &buf[..n]);
+    Ok(())
+}
+
+async fn write_all() -> io::Result<()> {
+    let mut f = File::create("hello.txt").await?;
+    f.write_all(b"Hello World").await?;
     Ok(())
 }
